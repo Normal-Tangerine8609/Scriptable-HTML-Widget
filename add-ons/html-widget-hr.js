5dff407ad5667c02a0d2d80b99355bcc90a8fe34
Normal-Tangerine8609 <82842683+Normal-Tangerine8609@users.noreply.github.com>
2022-03-17 13:00:52 -0400

    Update html-widget-hr.js

--- add-ons/html-widget-hr.js
+++ add-ons/html-widget-hr.js
@@ -1,71 +1,34 @@
 /*
-HTML Widget hr 1.1
+HTML Widget hr 2.0
 https://github.com/Normal-Tangerine8609/Scriptable-HTML-Widget/blob/main/add-ons/html-widget-hr.js
 
-- Added length attribute
-- Simplified output css
+- Compatible with HTML Widget 5.00
+- Removed vertical layout
+- Background attribute added and background-color and background-gradient are removed
 */
 module.exports = {
-  hr: {
-    isSelfClosing: true,
-    constructer: (incrementor, innerText, children, attrs, currentStack, finalCss) => {
-      let isHorizontal = true
-      for(let item of Object.keys(finalCss)) {
-        if(item == "layout-vertically"){
-          isHorizontal = false
-        } else if (item == "layout-horizontally") {
-          isHorizontal = true
-        }
-      }
-      let auto = (!finalCss["length"] && finalCss["length"]!= 0)?`\nhr${incrementor}.addSpacer()`:""
-      let width = finalCss["width"] || 1
-      let length = finalCss["length"] || 1
-      return `\nlet hr${incrementor} = ${currentStack}.addStack()${isHorizontal?"":`\nhr${incrementor}.layoutVertically()`}${auto}\nlet hrImage${incrementor} = hr${incrementor}.addImage(Image.fromData(Data.fromBase64String("iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=")))\nhrImage${incrementor}.imageSize = new Size(${isHorizontal?`${length},${width}`:`${width},${length}`})${auto}\nhr${incrementor}.backgroundColor = Color.dynamic(Color.black(), Color.white())`},
-    attr: {
-      "background-color": {
-        isBoolean: false,
-        isOnlyAttr: false,
-        func: async (value, incrementor, finalCss, Base) =>
-          await Base.colour("background-color", value, "hr" + incrementor),
-      },
-      "background-gradient": {
-        isBoolean: false,
-        isOnlyAttr: false,
-        func: async (value, incrementor, finalCss, Base) =>
-          await Base.gradient("background-gradient", value, "hr" + incrementor),
-      },
-      "url": {
-        isBoolean: false,
-        isOnlyAttr: false,
-        func: (value, incrementor, finalCss, Base) =>
-          `\nhr${incrementor}.url = "${value.replace(/"/g, "")}"`,
-      },
-      "corner-radius": {
-        isBoolean: false,
-        isOnlyAttr: false,
-        func: (value, incrementor, finalCss, Base) =>
-          Base.posInt("corner-radius", value, "hr" + incrementor),
-      },
-      "layout-horizontally": {
-        isBoolean: true,
-        isOnlyAttr: false,
-        func: (value, incrementor, finalCss, Base) => "",
-      },
-      "layout-vertically": {
-        isBoolean: true,
-        isOnlyAttr: false,
-        func: (value, incrementor, finalCss, Base) => "",
-      },
-      "width": {
-        isBoolean: false,
-        isOnlyAttr: false,
-        func: (value, incrementor, finalCss, Base) => {Base.posInt("width", value, "null", true); return ""},
-      },
-      "length": {
-        isBoolean: false,
-        isOnlyAttr: false,
-        func: (value, incrementor, finalCss, Base) => {Base.posInt("length", value, "null", true); return ""},
-      },
-    },
-  },
+  isSelfClosing: true,
+  func: async (validate, template, update, styles, attrs, innerText) => {
+    const mapping = {
+      "background": ["colour", "gradient", "image"],
+      "url": "url",
+      "corner-radius": "posInt",
+      "width": "posInt",
+      "hight": "posIng"
+    }
+
+    validate(attrs, styles, mapping)
+
+    await template(`
+    <stack background="${styles.background || "black-white"}" url="${
+      styles.url || "null"
+    }" corner-radius="${styles["corner-radius"] || "null"}">
+      ${styles.width ? "" : "<spacer>"}
+      <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" image-size="${
+        styles.width || 100
+      },${styles.hight || 1}">
+      ${styles.width ? "" : "<spacer>"}
+    </stack>
+  `)
+  }
 }
