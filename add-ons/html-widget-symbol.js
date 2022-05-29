53a518d888e9f894a466f3b6c9294ee47174ae69
Normal-Tangerine8609 <82842683+Normal-Tangerine8609@users.noreply.github.com>
2022-05-29 08:50:05 -0400

    Update html-widget-symbol.js

--- add-ons/html-widget-symbol.js
+++ add-ons/html-widget-symbol.js
@@ -1,9 +1,8 @@
 /*
-HTML Widget symbol 2.1
+HTML Widget symbol 2.2
 https://github.com/Normal-Tangerine8609/Scriptable-HTML-Widget/blob/main/add-ons/html-widget-symbol.js
 
-- Compatible with HTML Widget 5.00
-- Improved symbol image quality to match image-size
+- Compatible with HTML Widget 6.00
 */
 module.exports = async (
   validate,
@@ -30,13 +29,16 @@ module.exports = async (
   validate(attrs, styles, mapping)
   update(styles, mapping)
   
-  let symbol = SFSymbol.named(innerText || "questionmark.circle")
-  let symbolSize = 100
-  if(styles["image-size"]) {
-    let [width,height] = styles["image-size"].match(/\d+/g)
-    symbolSize = (width > height) ? height : width
+  let symbol = SFSymbol.named(innerText)
+  if(!symbol){
+    symbol = SFSymbol.named("questionmark.circle")
   }
-  symbol.applyFont(Font.systemFont(parseInt(symbolSize)))
+  let symbolSize = 100
+  if(styles["image-size"] !== "null") {
+    let [width,height] = styles["image-size"].match(/\d+/g)
+    symbolSize = parseInt((width > height) ? height : width)
+  }
+  symbol.applyFont(Font.systemFont(symbolSize))
   await template(`
 <img 
   src="data:image/png;base64,${Data.fromPNG(
@@ -53,6 +55,6 @@ module.exports = async (
   align-image="${styles["align-image"]}" 
   container-relative-shape="${styles["container-relative-shape"]}" 
   resizable="${styles["resizable"]}"
->
+/>
   `)
 }
