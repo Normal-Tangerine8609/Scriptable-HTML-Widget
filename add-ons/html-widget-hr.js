a2d7a018d65e33ac2f529820d8477eb88c92f421
Normal-Tangerine8609 <82842683+Normal-Tangerine8609@users.noreply.github.com>
2022-05-29 08:49:16 -0400

    Update html-widget-hr.js

--- add-ons/html-widget-hr.js
+++ add-ons/html-widget-hr.js
@@ -1,33 +1,37 @@
 /*
-HTML Widget hr 2.2
+HTML Widget hr 2.3
 https://github.com/Normal-Tangerine8609/Scriptable-HTML-Widget/blob/main/add-ons/html-widget-hr.js
 
-- Compatible with HTML Widget 5.00
+- Compatible with HTML Widget 6.00
 - Fixed height spelling
 */
-module.exports = {
-  isSelfClosing: true,
-  func: async (validate, template, update, styles, attrs, innerText) => {
-    const mapping = {
-      "background": ["colour", "gradient", "image"],
-      "url": "url",
-      "corner-radius": "posInt",
-      "width": "posInt",
-      "height": "posInt"
-    }
+module.exports = async (
+  validate,
+  template,
+  update,
+  styles,
+  attrs,
+  innerText
+) => {
+  const mapping = {
+    "background": ["colour", "gradient", "image"],
+    "url": "url",
+    "corner-radius": "posInt",
+    "width": "posInt",
+    "height": "posInt"
+  }
 
-    validate(attrs, styles, mapping)
+  validate(attrs, styles, mapping)
 
-    await template(`
+  await template(`
     <stack background="${styles.background || "black-white"}" url="${
-      styles.url || "null"
-    }" corner-radius="${styles["corner-radius"] || "null"}">
-      ${styles.width ? "" : "<spacer>"}
+    styles.url || "null"
+  }" corner-radius="${styles["corner-radius"] || "null"}">
+      ${styles.width ? "" : "<spacer/>"}
       <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" image-size="${
         styles.width || 100
-      },${styles.height || 1}">
-      ${styles.width ? "" : "<spacer>"}
+      },${styles.height || 1}"/>
+      ${styles.width ? "" : "<spacer/>"}
     </stack>
   `)
-  }
 }
