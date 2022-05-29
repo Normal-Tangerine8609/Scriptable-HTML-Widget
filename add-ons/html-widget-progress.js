948eb56de4d5b0b39dc63611736ad61fda4be22f
Normal-Tangerine8609 <82842683+Normal-Tangerine8609@users.noreply.github.com>
2022-05-29 08:49:40 -0400

    Update html-widget-progress.js

--- add-ons/html-widget-progress.js
+++ add-ons/html-widget-progress.js
@@ -1,48 +1,53 @@
 /*
-HTML Widget progress 1.2
+HTML Widget progress 1.3
 https://github.com/Normal-Tangerine8609/Scriptable-HTML-Widget/blob/main/add-ons/html-widget-progress.js
 
-- Compatible with HTML Widget 5.00
-- Errors updated to match main HTML Widget errors
+- Compatible with HTML Widget 6.00
+
 */
-module.exports = {
-  isSelfClosing: true,
-  func: async (validate, template, update, styles, attrs, innerText) => {
-    const mapping = {
-      "url": "url",
-      "background": ["colour", "gradient"],
-      "progress-background": ["colour", "gradient"],
-      "border-color": "colour",
-      "border-width": "posInt",
-      "corner-radius": "posInt",
-      "progress-corner-radius": "posInt",
-      "width": "posInt",
-      "height": "posInt",
-      "value": "decimal"
-    }
-    validate(attrs, styles, mapping)
-    let value = /\d*(?:\.\d*)?%?/.exec(attrs.value)[0]
-    if (value.endsWith("%")) {
-      value = Number(value.replace("%", ""))
-      value /= 100
-    }
-    if (!attrs.value) {
-      throw new Error("`progress` tag must have a `value` attribute")
-    }
-    if (value < 0) {
-      throw new Error(`\`value\` attribute must be above \`0\`: ${attrs.value}`)
-    }
-    if (value > 1) {
-      throw new Error(`\`value\` attribute must be below \`1\`: ${attrs.value}`)
-    }
-    
-    let width = Number(
+module.exports = async (
+  validate,
+  template,
+  update,
+  styles,
+  attrs,
+  innerText
+) => {
+  const mapping = {
+    "url": "url",
+    "background": ["colour", "gradient"],
+    "progress-background": ["colour", "gradient"],
+    "border-color": "colour",
+    "border-width": "posInt",
+    "corner-radius": "posInt",
+    "progress-corner-radius": "posInt",
+    "width": "posInt",
+    "height": "posInt",
+    "value": "decimal"
+  }
+  validate(attrs, styles, mapping)
+  let value = /\d*(?:\.\d*)?%?/.exec(attrs.value)[0]
+  if (value.endsWith("%")) {
+    value = Number(value.replace("%", ""))
+    value /= 100
+  }
+  if (!attrs.value) {
+    throw new Error("`progress` tag must have a `value` attribute")
+  }
+  if (value < 0) {
+    throw new Error(`\`value\` attribute must be above \`0\`: ${attrs.value}`)
+  }
+  if (value > 1) {
+    throw new Error(`\`value\` attribute must be below \`1\`: ${attrs.value}`)
+  }
+
+  let width = Number(
     styles.width && styles.width !== "null" ? styles.width : 100
   )
   let height = Number(
     styles.height && styles.height !== "null" ? styles.height : 1
   )
-    await template(`
+  await template(`
     <stack url="${styles.url || "null"}" 
       background="${styles.background || "black-white"}" 
       border-color="${styles["border-color"] || "null"}" 
@@ -52,12 +57,11 @@ module.exports = {
     >
       <stack background="${styles["progress-background"] || "gray"}" 
         corner-radius="${styles["progress-corner-radius"] || "null"}" 
-        size="${
-      Math.round(width * Number(value))
-    }, ${height}">
+        size="${Math.round(width * Number(value))}, ${height}">
       </stack>
-      <stack size="${Math.round(width * (1 - Number(value)))}, ${height}"></stack>
+      <stack size="${Math.round(
+        width * (1 - Number(value))
+      )}, ${height}"></stack>
     </stack>
   `)
-  }
 }
